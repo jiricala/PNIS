e77bd393e6f1be2ed52493786dd6fb2f0cd01c41
Jiří Čala <jiri.cala@gmail.com>
2024-12-18 11:13:08 +0100

    update output files in multiplier folder

--- multiplier/verilog/multiplier.sv
+++ multiplier/verilog/multiplier.sv
@@ -1,77 +1,52 @@
 `timescale 1ns / 1ps
 
-module multiplier(
-        input  logic        RST,
-        input  logic        CLK,
-        input  logic        EN_i,
-        input  logic [31:0] DIN1_i,
-        input  logic [31:0] DIN2_i,
-        output logic        BUSY_o,
-        output logic        VALID_o,
-        output logic [63:0] DOUT_o
+module multiplier (
+    input  logic [31:0] DIN1_i,
+    input  logic [31:0] DIN2_i,
+    output logic [63:0] DOUT_o
+);
+
+    // ---------------------------------------
+
+    // internal signals
+    logic [32:0] rca_in1 [0:31];
+    logic [32:0] rca_in2 [0:31];
+    logic [32:0] rca_out [0:31];
+    logic        c_in    [0:31];
+    logic        c_out   [0:31];
+    logic [63:0] data_out;
+    
+    // ---------------------------------------
+    
+    // 32 x 33bit-adder instantiation
+    adder_RCA_33b adders[0:31](
+        .C_i     ( c_in    ),
+        .A_33b_i ( rca_in1 ),
+        .B_33b_i ( rca_in2 ),
+        .S_33b_o ( rca_out ),
+        .C_o     ( c_out   )          // c_out unused
     );
     
-    reg   [5:0] cntr;
-    reg         busy;
-    reg         valid;
-    
-    reg  [31:0] din1_reg;
-    reg  [31:0] din2_sreg;
-    reg  [63:0] prod_sreg;
-    
-    wire [31:0] sum_in1;
-    wire [31:0] sum_in2;
-    wire [32:0] sum_out;
-    
-    //--- INPUT DATA REGISTERS & SHIFT REGISTERS ------------------------------
-    always @(posedge CLK) begin
-        if (RST) begin
-            din1_reg  <= DIN1_i;
-            din2_sreg <= DIN2_i;
-            prod_sreg <= 32'b0;
-        end else if (busy) begin
-            din2_sreg <= {1'b0, din2_sreg[31:1]};       // shift-register for data2 input
-            prod_sreg <= {sum_out, prod_sreg[31:1]};    // half shift-register for adder results
-        end else if (EN_i) begin
-            din1_reg  <= DIN1_i;
-            din2_sreg <= DIN2_i;
-            prod_sreg <= 32'b0;
+    // wiring logic for adders' inputs/outputs
+    genvar i;
+    generate
+        for (i = 0; i < 31; i++) begin
+            assign rca_in1[i] = (DIN2_i[i]) ? {DIN1_i[31], DIN1_i} : 33'b0;
+            assign c_in[i] = 1'b0;
+            assign rca_in2[i + 1] = {rca_out[i][32], rca_out[i][32:1]};
+            assign data_out[i] = rca_out[i][0];
         end
-    end
+    endgenerate
+
+    // initial addition input2
+    assign rca_in2[0] = 33'b0;
     
-    //--- CONTROL SIGNALS -------------------------------------------------------
-    always @(posedge CLK) begin
-        if (RST) begin
-            busy  = 1'b0;
-            valid = 1'b0;
-        end else if (cntr[5]) begin         // if 32 cycles completed
-            busy  = 1'b0;
-            valid = 1'b1;
-        end else if (EN_i) begin            // if new multiplication initiated
-            busy  = 1'b1;
-            valid = 1'b0;
-        end
-        
-        //--- internal counter ---
-        if (RST || cntr[5])
-            cntr = 0;
-        else if (EN_i || busy)
-            cntr++;
-    end
-    
-    adder32b adder(
-            .A_32b( sum_in1 ), 
-            .B_32b( sum_in2 ), 
-            .S_32b( sum_out[31:0] ), 
-            .Cout( sum_out[32] ) 
-        );
-        
-    assign sum_in1 = din2_sreg[0] ? din1_reg : 32'b0;
-    assign sum_in2 = prod_sreg[63:32];
-    
-    //--- OUTPUT SIGNALS --------------------------------------------------------
-    assign BUSY_o  = busy;
-    assign VALID_o = valid;
-    assign DOUT_o  = prod_sreg;
-    
+    // last addition input1 logic
+    assign rca_in1[31] = (DIN2_i[31]) ? ~{DIN1_i[31], DIN1_i} : 33'b0;      // inversion for 2's complement
+    assign c_in[31] = DIN2_i[31];                                           // add +1 for 2's complement
+
+    // output
+    assign data_out[63:31] = rca_out[31];
+    assign DOUT_o = data_out;
+
 endmodule
